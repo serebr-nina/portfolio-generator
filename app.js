f873bb1eef7f4cbf5d3e801b87982924e1950728
NinaSerebryakova <serebr.nina@gmail.com>
2022-09-17 17:47:08 -0400

    added initial HTML file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-console.log('Hello Node!');
-const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+/*const printProfileData = profileDataArr => {
     // This...
     for (let i = 0; i < profileDataArr.length; i += 1) {
         console.log(profileDataArr[i]);
@@ -14,3 +15,24 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+*/
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+};
+console.log(name, github);
+console.log(generatePage(name, github));
