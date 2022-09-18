5e6aff677bd1776aad13815b288d432b1fb2d56c
NinaSerebryakova <serebr.nina@gmail.com>
2022-09-17 20:25:42 -0400

    finished creating a new portfolio web page, modularized the app by moving HTML generator into its own file

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2); //, process.argv.length);
 const [name, github] = profileDataArgs;
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 /*const printProfileData = profileDataArr => {
     // This...
     for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -16,23 +21,3 @@ const [name, github] = profileDataArgs;
 
 printProfileData(profileDataArgs);
 */
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
-console.log(name, github);
-console.log(generatePage(name, github));
